59f8185c1d9546c079ab971f398fb764bcaff30b
Ben Levinsky <ben.levinsky@xilinx.com>
2019-06-26 14:54:00 -0700

    add Choice field in admin section
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>

--- django_app/mysite/polls/admin.py
+++ django_app/mysite/polls/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
-admin.site.register(Question)
+from .models import Question, Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
 
